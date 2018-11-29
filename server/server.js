d8385b1aed5b12bc4f29663b1faae43090b40bf7
stwingy <42314335+stwingy@users.noreply.github.com>
2018-11-29 07:28:04 +0000

    added mongolab

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const {auth} =require('./middleware/auth')
 const app = express();
 
 mongoose.Promise = global.Promise
-mongoose.connect('mongodb://localhost:27017/auth')
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/auth')
 const {User}= require('./models/user')
 app.use(bodyParser.json())
 app.use(cookieParser())
